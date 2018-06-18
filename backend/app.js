3cfef4e2127913dccdbd64fcd7547410f0239478
Andy <andrewpeliza@hotmail.com>
2018-06-19 00:26:04 +0100

    Http requests to dummy data working. Next step add Mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/api/posts', (req, res, next) => {
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+);
+res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"
+);
+  next();
+});
+// N2LcfyylMOLfsbel
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added succesfully'
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
   const posts = [
     {
       id: 'fd2343534l',
@@ -21,7 +43,5 @@ app.use('/api/posts', (req, res, next) => {
     posts: posts
   });
 });
-
-
 
 module.exports = app;
