6433d072c06165a3be4305bddda033658f53542d
Andy <andrewpeliza@hotmail.com>
2018-06-18 19:35:53 +0100

    Backend server with node/express with added dummy data

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'fd2343534l',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+  },
+    {
+      id: 'kjsd334543',
+      title: 'Second server-side post',
+      content: 'This is coming from the server!'
+  }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched succesfully!',
+    posts: posts
+  });
+});
+
+
+
+module.exports = app;
